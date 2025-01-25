04a7357e58f9b048aaa391f4ffa24c5029e4dada
Nada Gamal <nadoy20_8@hotmail.com>
2025-01-25 23:01:38 +0200

    Add base structure for ChessLogic and ChessUI
    - Added Player enum with turn-switching functionality.
    - Implemented Position class with row/column logic and overridden operators.
    - Created Direction class for movement calculations.
    - Added basic assets for ChessUI (icons and board images).

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Direction
+    {
+        public int RowDelta { get;  }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction dir1, Direction dir2)
+        {
+            return new Direction(dir1.RowDelta + dir2.RowDelta, dir1.ColumnDelta + dir2.ColumnDelta);
+        }
+    }
+}
