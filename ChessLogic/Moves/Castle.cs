7d5ea79d367ef0752e4def35b925b1f9126bd234
Nada Gamal <nadoy20_8@hotmail.com>
2025-02-05 04:40:52 +0200

    Added Castling moves.
    - Added Castle class that inherits from Move.
    - Modified King class to support castling when applicable.

---
+++ ChessLogic/Moves/Castle.cs
@@ -0,0 +1,73 @@
+﻿namespace ChessLogic
+{
+    public class Castle : Move
+    {
+        /* rules for castling:
+           1. king hasn't moved.
+           2. rook hasn't moved.
+           3. squares between king and rook are empty.
+           4. if king is in check, king cannot castle.
+           5. king may not move into or through check.
+        */
+        public override MoveType Type { get; }
+        public override Position FromPos { get; }
+        public override Position ToPos { get; }
+
+        private readonly Direction kingMoveDir;
+        private readonly Position rookFromPos;
+        private readonly Position rookToPos;
+
+        public Castle(MoveType type, Position kingPos)
+        {
+            Type = type;
+            FromPos = kingPos;
+
+            if (type == MoveType.CastleKS)
+            {
+                kingMoveDir = Direction.East;
+                ToPos = new Position(kingPos.Row, 6);
+                rookFromPos = new Position(kingPos.Row, 7);
+                rookToPos = new Position(kingPos.Row, 5);
+            }
+            else if (type == MoveType.CastleQS) 
+            {
+                kingMoveDir = Direction.West;
+                ToPos = new Position(kingPos.Row, 2);
+                rookFromPos = new Position(kingPos.Row, 0);
+                rookToPos = new Position(kingPos.Row, 3);
+            } 
+        }
+
+        public override void Execute(Board board)
+        {
+            new NormalMove(FromPos, ToPos).Execute(board);
+            new NormalMove(rookFromPos, rookToPos).Execute(board);  
+        }
+
+        public override bool IsLegal(Board board)
+        {
+            Player player = board[FromPos].Color;
+
+            if (board.IsInCheck(player))
+            {
+                return false;
+            }
+
+            Board copy = board.Copy();
+            Position kingPosInCopy = FromPos;
+
+            for (int i = 0; i < 2; i++)
+            {
+                new NormalMove(kingPosInCopy, kingPosInCopy + kingMoveDir).Execute(copy);
+                kingPosInCopy += kingMoveDir; 
+                
+                if (copy.IsInCheck(player) ) 
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51d0efd52b2cb87b0f4ae115b34e011a90c06b8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b856793bda65ea9a4db27acc58ee17e9b369e0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
